65e89c76465f942d2865a69342ebac4dcc6056a3
Zbigniew Zasieczny <z.zasieczny@gmail.com>
2021-07-20 01:47:11 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import task_2020_01
+import task_2020_02
+import task_2020_03
+import task_2020_04
+import task_2020_05
+import task_2020_06
+import task_2020_07
+
+if __name__ == '__main__':
+    print("\ntask 1.1:")
+    task_2020_01.get_solution_1()
+
+    print("\ntask1.2:")
+    task_2020_01.get_solution_2()
+
+    print("\ntask2.1")
+    task_2020_02.get_solution_1()
+
+    print("\ntask2.2")
+    task_2020_02.get_solution_2()
+
+    print("\ntask3.1")
+    task_2020_03.get_solution_1()
+
+    print("\ntask3.2")
+    task_2020_03.get_solution_2()
+
+    print("\ntask4.1")
+    task_2020_04.get_solution_1()
+    print("\ntask4.2")
+    task_2020_04.get_solution_2()
+
+    print("\ntask5.1")
+    task_2020_05.get_solution_1()
+    print("\ntask5.2")
+    task_2020_05.get_solution_2()
+
+    print("\ntask6.1")
+    task_2020_06.get_solution_1()
+    print("\ntask6.2")
+    task_2020_06.get_solution_2()
+
+    print("\ntask7.1")
+    task_2020_07.get_solution_1()
+    print("\ntask7.2")
+    task_2020_07.get_solution_2()
